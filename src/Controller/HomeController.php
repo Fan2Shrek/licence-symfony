ab8b25be1ed4f5b280d7da2bfd911678ae26ee58
Fan2Shrek <pierre27.ambroise@gmail.com>
2024-02-25 11:23:37 +0100

    Clear some code

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -7,14 +7,17 @@ use App\Repository\ProjectRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\DependencyInjection\Attribute\AutowireCallable;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'home')]
-    public function index(ProjectRepository $projectRepository): Response
+    public function index(
+        #[AutowireCallable(service: ProjectRepository::class, method: 'findBy')] \Closure $project
+    ): Response
     {
         return $this->render('home/index.html.twig', [
-            'projects' => $projectRepository->findBy([], ['id' => 'DESC'])
+            'projects' => $project([], ['id' => 'DESC'])
         ]);
     }
 
